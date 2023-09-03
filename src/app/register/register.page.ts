bc18210bc26db72ce07fb416386ac25acf40af6f
RgokuD7 <richarddiaz0107@gmail.com>
2023-09-02 20:57:17 -0400

    Avance Validación

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController 
+    ) { }
 
+  user: any = {
+    nombres: '',
+    apellidos: '',
+    fec_nac: '',
+    sexo: '',
+    dep_fav: '',
+    tel: '',
+    email: '',
+    username: '',
+    password: ''
+  }
+  pass_confirmation: string  = ''
+
+  NombreIngresado: boolean = false;
+  NombreValido: boolean = false;
+  ApellidoIngresado: boolean = false;
+  ApellidoValido: boolean = false;
+  FechaIngresada: boolean = false;
+  FechaValida: boolean = false;
+  SexoIngresado: boolean = false;
+  DeporteIngresado: boolean = false;
+  TelefonoIngresado: boolean = false;
+  TelefonoValido: boolean = false;
+  EmailIngresado: boolean = false;
+  EmailValido: boolean = false;
+  UsuarioIngresado: boolean = false;
+  UsuarioExistente: boolean = false;
+  ContraseniaIngresada: boolean = false;
+  ContraseniaValida: boolean = false;
+  ConfContraseniaIngresada: boolean = false;
+  ConfContraseniaValida: boolean = false;
   ngOnInit() {
   }
 
   onSubmit(){
+    console.log(this.user);
+    console.log(this.pass_confirmation);
+    
     
   }
+  
+  onWillDismiss() {
+    this.navCtrl.navigateForward('login');
+  }
+  
 
 }
